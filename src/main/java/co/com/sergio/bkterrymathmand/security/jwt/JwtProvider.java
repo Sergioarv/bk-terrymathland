54e66c1e9e31581748f06d94b8399653a6a11f53
SERGIO RODRÍGUEZ <SARV9208@hotmail.com>
2022-07-09 10:50:48 -0500

    Correccion de refresh en jwt

--- src/main/java/co/com/sergio/bkterrymathmand/security/jwt/JwtProvider.java
+++ src/main/java/co/com/sergio/bkterrymathmand/security/jwt/JwtProvider.java
@@ -6,9 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtProvider {
@@ -23,8 +26,11 @@ public class JwtProvider {
 
     public String generateToken(Authentication authentication) {
         UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
+        List<String> roles = usuarioPrincipal.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
         return Jwts.builder()
                 .setSubject(usuarioPrincipal.getUsername())
+                .claim("roles", roles)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + expiration * 1000))
                 .signWith(SignatureAlgorithm.HS512, secret.getBytes())
